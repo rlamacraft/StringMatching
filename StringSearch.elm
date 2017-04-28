91a57eeefeff6f804639138f0dddeecaa1e4c4c4
Robert <rlamacraft@gmail.com>
2017-04-28 16:29:27 +0100

    Refactored and commented

--- StringSearch.elm
+++ StringSearch.elm
@@ -3,13 +3,21 @@ module StringSearch exposing (borderTable,searchString,State(..))
 import Array exposing (Array(..),length,get,push,fromList)
 import String exposing (uncons,dropLeft,length)
 
+{-| Implementation of [Morris-Pratt String Searching](https://en.wikipedia.org/wiki/Knuth%E2%80%93Morris%E2%80%93Pratt_algorithm).
+
+# Searching
+@docs searchString, match, shiftPattern
+
+# Border Table
+@docs borderTable, newBorder, borderOfBorderLoop
+-}
+
 type State
   = Failed String
   | Match
   | NoMatch
 
--- FIND SUBSTRING USING BORDER TABLE
-
+{-| Recursive searching of a pattern on a text, using a precomputed border table -}
 searchString : String -> String -> Result String (Array Int) -> Int -> State
 searchString text pattern borderTable patternIndex =
   case borderTable of
@@ -20,7 +28,7 @@ searchString text pattern borderTable patternIndex =
         case match text pattern patternIndex of
           Match ->
             if patternIndex == String.length pattern - 1 then
-              Match
+              Match -- only attempts to find first occurance
             else
               searchString (dropLeft 1 text) pattern (Ok table) (patternIndex + 1)
           NoMatch ->
@@ -30,6 +38,7 @@ searchString text pattern borderTable patternIndex =
     Err errorText ->
       Failed errorText
 
+{-| Checks if a specified character of the pattern matches the first character of the text -}
 match : String -> String -> Int -> State
 match text pattern patternIndex =
   let
@@ -49,6 +58,7 @@ match text pattern patternIndex =
       Nothing ->
         Failed "indexing pattern error"
 
+{-| Uses the border table to shift the window through which the pattern is compared against the text -}
 shiftPattern : String -> String -> Array Int -> Int -> Int
 shiftPattern text pattern borderTable patternIndex =
   let
@@ -67,57 +77,46 @@ shiftPattern text pattern borderTable patternIndex =
     else
       patternIndex
 
--- CALCULATE THE BORDER TABLE
-
+{-| Recursively calculates the border table for a given pattern -}
 borderTable : Array Int -> String -> Result String (Array Int)
 borderTable table pattern =
   let
-    endingChar = uncons (dropLeft (Array.length table - 1) pattern)
+    nextChar = uncons (dropLeft (Array.length table - 1) pattern)
   in
     if Array.length table == 0 then
       borderTable (push -1 table) pattern
     else if (String.length pattern) + 1 == (Array.length table) then
       Ok table
     else
-      case endingChar of
-        Just (char,tail) ->
+      case nextChar of
+        Just (char,tail) -> --not end of pattern
           case newBorder pattern table char of
-            Ok newBorderValue -> borderTable (push newBorderValue table) pattern
+            Ok newBorderValue -> borderTable (push (newBorderValue + 1) table) pattern
             Err error -> Err error
         Nothing ->
           Ok (fromList [])
 
+{-| Finds the next value of the border table -}
 newBorder : String -> Array Int -> Char -> Result String Int
 newBorder pattern table endingChar =
-  let
-    lastBorder = get ((Array.length table) - 1) table
-  in
-    case lastBorder of
-      Just num ->
-        case borderOfBorderLoop pattern table endingChar num of
-          Ok value -> Ok (value + 1)
-          Err error -> Err error
-      Nothing ->
-        Err "table indexing error"
+  borderOfBorderLoop pattern table endingChar
+    <| get ((Array.length table) - 1) table
 
-
-borderOfBorderLoop : String -> Array Int -> Char -> Int -> Result String Int
-borderOfBorderLoop pattern table endingChar tmp =
-  let
-    char = uncons (dropLeft tmp pattern)
-  in
-    if tmp < 0 then
-      Ok tmp
-    else
-      case char of
-        Just (headc,tailc) ->
-          if headc == endingChar then
-            Ok tmp
-          else
-            case (get tmp table) of
-              Just newTmp ->
-                borderOfBorderLoop pattern table endingChar newTmp
-              Nothing ->
-                Err "tmp indexing table error"
-        Nothing ->
-          Err "tmp indexing pattern error"
+{-| Finds the next value of the border table -}
+borderOfBorderLoop : String -> Array Int -> Char -> Maybe Int -> Result String Int
+borderOfBorderLoop pattern table endingChar lastBorder =
+  case lastBorder of
+    Just lastBorderValue ->
+      if lastBorderValue < 0 then
+        Ok lastBorderValue --break loop if -1 has been reached as it is the minimum value which is the max shift
+      else
+        case uncons (dropLeft lastBorderValue pattern) of
+          Just (headc,tailc) ->
+            if headc == endingChar then
+              Ok lastBorderValue --break loop if equal then border of border has been found
+            else
+              borderOfBorderLoop pattern table endingChar (get lastBorderValue table)
+          Nothing ->
+            Err "indexing pattern error"
+    Nothing ->
+      Err "indexing table error"
